22cfeb637c8d29d9bdc206e433d05ac9d19f5990
SakethK <sakethk813@gmail.com>
2025-11-12 21:31:22 +0530

    total done

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -27,12 +27,9 @@ data "aws_availability_zones" "available" {}
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.8.0"
-
-  cluster = {
-    name    = var.cluster_name
-    version = "1.29"
-  }
-
+  cluster_name   = var.cluster_name
+  cluster_version = "1.29"
+  
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
