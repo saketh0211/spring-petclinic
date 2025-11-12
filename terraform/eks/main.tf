9112f0d846019059f1e69e84e49634a02499d7e2
SakethK <sakethk813@gmail.com>
2025-11-12 20:42:20 +0530

    latest

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,13 +1,13 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.2"
+  version = "6.5.0"
 
   name = "${var.cluster_name}-vpc"
-  cidr = "10.100.0.0/16"
+  cidr = "10.0.0.0/16"
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnets  = ["10.100.1.0/24", "10.100.2.0/24"]
-  private_subnets = ["10.100.10.0/24", "10.100.11.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -17,10 +17,10 @@ data "aws_availability_zones" "available" {}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "25.0.0"
+  version = "21.8.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.28"
+  cluster_version = "1.29"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
