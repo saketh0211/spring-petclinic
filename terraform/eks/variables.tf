d7e7eb5655bd0b0358f00de74daf7eaa79a0d743
SakethK <sakethk813@gmail.com>
2025-11-12 20:44:53 +0530

    mom

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,7 +1,34 @@
-variable "aws_region" { type = string, default = "ap-south-1" }
-variable "cluster_name" { type = string, default = "saketh-eks-cluster" }
-variable "node_group_name" { type = string, default = "saketh-eks-nodes" }
-variable "desired_capacity" { type = number, default = 2 }
-variable "min_size" { type = number, default = 1 }
-variable "max_size" { type = number, default = 3 }
-variable "instance_type" { type = string, default = "t3.medium" }
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "saketh-eks-cluster"
+}
+
+variable "node_group_name" {
+  type    = string
+  default = "saketh-eks-nodes"
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 3
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.medium"
+}
