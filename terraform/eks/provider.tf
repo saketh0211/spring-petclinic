fffe8c69c147d182460087561ba2bf52f69d29d6
SakethK <sakethk813@gmail.com>
2025-11-12 20:48:54 +0530

    added jenkins file

--- terraform/eks/provider.tf
+++ terraform/eks/provider.tf
@@ -1,11 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "infrapipelinejenkins"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
-}
-terraform {
-  backend "s3" {
-    bucket         = "infrapipelinejenkins"   # Replace with your S3 bucket name
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
 }
--- terraform/eks/versions.tf
+++ terraform/eks/versions.tf
@@ -1,9 +1,3 @@
 terraform {
-  required_version = ">= 1.2"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+  required_version = ">= 1.3.0"
 }
